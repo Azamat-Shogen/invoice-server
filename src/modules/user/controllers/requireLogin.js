5dc6832fe989c5781054c31de8aeb3461ee2563c
Azamat Shogen <baki47hanma@gmail.com>
2022-05-16 23:42:15 -0500

    auth 2nd solution added

--- src/modules/user/controllers/requireLogin.js
+++ src/modules/user/controllers/requireLogin.js
@@ -1,8 +1,31 @@
 import { expressjwt } from 'express-jwt';
+import jwt from 'jsonwebtoken';
 
-const requireLogin = expressjwt({ // req.user
-    secret: process.env.JWT_SECRET,
-    algorithms: ['HS256'], 
-});
+// const requireLogin = expressjwt({ // req.user
+//     secret: process.env.JWT_SECRET,
+//     algorithms: ['HS256'], 
+// });
+
+// export default requireLogin;
+
+
+// second solution / auth:
+
+const requireLogin = async ( req, res, next) => {
+
+    const token = req.headers.authorization.split(' ')[1];
+    if(!token) return res.status(401).json({error: 'Access denied'});
+
+    try {
+       
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        req.auth = verified;
+        next();
+
+    } catch (error){
+        console.log(error);
+        return res.status(400).json({error});
+    }
+};
 
 export default requireLogin;
